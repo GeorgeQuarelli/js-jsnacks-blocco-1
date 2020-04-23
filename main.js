8ca32c451013024c9dd6b7a152a5bc287686c1f3
George <quarelli.george@gmail.com>
2020-04-24 01:23:27 +0200

    Jsnack5

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 //L'utente dice un numero N e stampo il cubo di tot numeri fino ad N
+
+var numeri_utente = parseInt(prompt("Calcolo del cubo fino al numero:"))
+if (isNaN(numeri_utente) == true) {
+    console.log("errore");
+} else {
+    for (var i = 1; i <= numeri_utente; i++) {
+        console.log(i*i*i);
+    }
+}
